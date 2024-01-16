96ff483f5c8c70b05e76c7d51c4105d177b27378
Explorarthecreator <fredadejiemma99@gmail.com>
2024-01-16 17:28:37 +0100

    Created the Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Offers from './Pages/Offers'
+import Explore from './Pages/Explore'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import ForgotPassword from './Pages/ForgotPassword'
 
 
 function App() {
   return (
     <>
-      <h1>
-        My App
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/profile" element={<SignIn/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+        </Routes>
+        {/* Navbar */}
+        <Navbar/>
+      </Router>
       
     </>
   );
